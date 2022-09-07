1281ef26164ea520583b82fa2d22dc0600f47893
Marshall Mao <marshall.mao@viljasolutions.com>
2022-09-07 20:18:40 +0800

    classic druid

--- druid/balance.lua
+++ druid/balance.lua
@@ -1,4 +1,5 @@
 local ydyb_cast;
+local moontime = 0;
 function ydybBalancePlay(ydyb, f, g)
     --buff
     local moon = mwGetBuffTime("月蚀");
@@ -6,21 +7,28 @@ function ydybBalancePlay(ydyb, f, g)
     local debuff_moonlight = mwGetDebuffTime("月火术");
     local cd_jihuo = mwGetCoolDown("激活");
     local cd_item = mwGetItemCoolDown(25634);
+
+    if ydyb.now-moontime > 15 then
+        ydyb_cast = "愤怒";
+    end
     --single
     if cd_jihuo<=ydyb.cd_gcd and ydyb.focus<=500 then
         --激活
         f.textures[0]:SetColorTexture(0, 1, 0); --浅粉色 旋风斩 7
     elseif sun>0.1 then
-        f.textures[0]:SetColorTexture(1, 0, 0);
         ydyb_cast = "愤怒";
         if cd_item<=0 then
             f.textures[0]:SetColorTexture(1, 1, 0);
+        else
+            f.textures[0]:SetColorTexture(1, 0, 0);
         end
     elseif moon>0.1 then
-        f.textures[0]:SetColorTexture(0, 0, 1);
+        moontime=GetTime();
         ydyb_cast = "星火术";
         if cd_item<=0 then
             f.textures[0]:SetColorTexture(1, 0, 1);
+        else
+            f.textures[0]:SetColorTexture(0, 0, 1);
         end
     elseif moon==0 and sun==0 and debuff_moonlight==0 then
         f.textures[0]:SetColorTexture(0, 1, 1);
