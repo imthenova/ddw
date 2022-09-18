f8cc2a858d18eb99b65dec3bbc736efb763c9fac
marmao <marshall.mao@viljasolutions.com>
2022-09-18 20:42:43 +0800

    fury

--- warriors/fury.lua
+++ warriors/fury.lua
@@ -4,7 +4,7 @@ function dsdtFuryPlay(dstd,f,g)
 			--print(buff_bonegrinder);
 			--execute
 			local isZhanShaPeriod = false;
-			if dstd.tPerHealth<=35 or dstd.tPerHealth>=180 then
+			if dstd.tPerHealth<=20 or dstd.tPerHealth>=180 then
 				isZhanShaPeriod = true;
 			end
 			--单体
@@ -20,24 +20,22 @@ function dsdtFuryPlay(dstd,f,g)
 				end
 			elseif (dstd.buff_jn<=dstd.cd_gcd and dstd.rage>=80) or (dstd.buff_lumang>0 and dstd.rage>=80) or dstd.rage>=90 then
 				f.textures[0]:SetColorTexture(0, 1, 1) --青 暴怒 4
-			elseif dstd.buff_lumang >0.1 and dstd.cd_panzui<=dstd.cd_gcd and (isZhanShaPeriod or dstd.buff_cs>0.5) then -- 斩杀阶段有激怒
-				f.textures[0]:SetColorTexture(1, 0, 0) --红 斩杀 z
-			elseif dstd.buff_jn >0.1 and dstd.cd_panzui<=dstd.cd_gcd and (isZhanShaPeriod or dstd.buff_cs>0.5) then -- 斩杀阶段有激怒
-				f.textures[0]:SetColorTexture(1, 0, 0) --红 斩杀 z
+			elseif dstd.currentCharges_nj>=1 and dstd.buff_lumang >0.1 then
+				f.textures[0]:SetColorTexture(0.4, 0, 0.4) --黄 怒击 3
 			elseif dstd.cd_panzui<=dstd.cd_gcd and (isZhanShaPeriod or dstd.buff_cs>0.5) then -- 斩杀阶段没激怒
 				f.textures[0]:SetColorTexture(1, 0, 0) --红 斩杀 z
+			elseif dstd.currentCharges_nj>=3 then
+				f.textures[0]:SetColorTexture(0.4, 0, 0.4) --黄 怒击 3
 			elseif dstd.buff_jn<=0 and dstd.cd_sx<=dstd.cd_gcd then
 				f.textures[0]:SetColorTexture(0, 0, 0) --黑 嗜血 2
 			--elseif isAuto and dstd.tHealth>200000 and dstd.buff_jn>2 and dstd.buff_lumang<=0 and dstd.cd_jrfb<=dstd.cd_gcd and dstd.rage<=50 then
 			--	f.textures[0]:SetColorTexture(0, 0.4, 0.4) --蓝 剑刃风暴 e
 			--elseif isAuto and dstd.buff_jn>0.1 and dstd.cd_spear<=dstd.cd_gcd then
 			--	f.textures[0]:SetColorTexture(0, 0, 1) --蓝 巨龙 e
-			elseif dstd.currentCharges_nj>=2 then
+			elseif dstd.currentCharges_nj>=1 then
 				f.textures[0]:SetColorTexture(0.4, 0, 0.4) --黄 怒击 3
 			elseif dstd.cd_sx<=dstd.cd_gcd then
 				f.textures[0]:SetColorTexture(0, 0, 0) --黑 嗜血 2
-			elseif dstd.currentCharges_nj>=1 then
-				f.textures[0]:SetColorTexture(0.4, 0, 0.4) --黄 怒击 3
 			else
 				f.textures[0]:SetColorTexture(1, 0.8, 0.8) --藏青 旋风斩
 			end
